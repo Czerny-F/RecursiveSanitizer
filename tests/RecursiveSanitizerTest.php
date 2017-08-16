64d62204d1655586b4c11a265e7113eb6bc53685
Czerny <dummy00.cz@gmail.com>
2017-08-16 12:58:48 +0900

    test trimmer

--- tests/RecursiveSanitizerTest.php
+++ tests/RecursiveSanitizerTest.php
@@ -2,7 +2,38 @@
 
 namespace Czerny;
 
+use PHPUnit\Framework\TestCase;
+
 class RecursiveSanitizerTest extends TestCase {
-  public function test() {
+
+  public function setUp() {
+    $this->trimmer = new RecursiveSanitizer('trim');
+  }
+
+  public function testTrimmerWithString() {
+    $this->assertEquals('foo', $this->trimmer->execute(' foo '));
+  }
+
+  public function testTrimmerWithArray() {
+    $this->assertEquals(
+      ['foo', 'bar', 'baz'],
+      $this->trimmer->execute([' foo', 'bar ', ' baz '])
+    );
+  }
+
+  public function testTrimmerWithAssocArray() {
+    $this->assertEquals(
+      ['foo' => 'bar', ' baz ' => 'hoge'],
+      $this->trimmer->execute(['foo' => ' bar ', ' baz ' => '    hoge   '])
+    );
+  }
+
+  public function testTrimmerWithMultiDimensionalArray() {
+    $this->assertEquals(
+      ['foo', ['bar', 'baz'], ['hoge', ['hogehoge', 'hoge hoge']]],
+      $this->trimmer->execute(
+        [' foo', ['bar ', ' baz '], ['  hoge ', ['   hogehoge   ', ' hoge hoge  ']]]
+      )
+    );
   }
 }
